a092f3a53342bbbec8c4057fd24c64abc079cd67
Mira Tamiru <miratamiru774@gmail.com>
2026-08-26 16:39:37 +0300

    string.py

---
+++ string.py
@@ -0,0 +1,27 @@
+single = 'Hello, world!'
+print(single)
+
+double = "It's a sunny day in Addis Ababa"
+print(double)
+
+mixed1 = 'She said, "Python is fun!"'
+print(mixed1)
+
+mixed2 = "It's a great day, isn't it?"
+print(mixed2)
+
+triple = """This is line one.
+This is line two.
+This is line three."""
+print(triple)
+
+def greet(name):
+    """This function greets the person passed in as a parameter."""
+    return f"Hello, {name}!"
+
+print(greet("Mira"))
+
+triple_single = '''Another way
+to write
+multi-line strings.'''
+print(triple_single)
