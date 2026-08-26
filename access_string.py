402a7dc50d4b39860f24392112c9cc18403f590c
Mira Tamiru <miratamiru774@gmail.com>
2026-08-26 17:15:37 +0300

    accessing string in python

---
+++ access_string.py
@@ -0,0 +1,14 @@
+text = "Shaggar Institute"
+
+print(text[0])
+print(text[7])
+
+print(text[-1])
+print(text[-2])
+
+print(text[0:7])
+print(text[8:])
+print(text[:7])
+print(text[-10:])
+print(text[::-1])
+print(text[0:7:2])
